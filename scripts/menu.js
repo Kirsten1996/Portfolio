3e40fbccd2e108b76754b36803a2c50f4f10d2a8
kirsten <kirsten@bnry.digital>
2018-04-16 15:45:25 +0200

    portfolio

---
+++ scripts/menu.js
@@ -0,0 +1,11 @@
+function myFunction () {
+  document.querySelector(".myNav").classList.toggle("isOpen");
+}
+
+var display = document.getElementById("icon");
+display.addEventListener("click", myFunction);
+
+
+
+
+cls
